efb952774fcd195d7b1e0d0d4f57ec18bbfd3f95
Anthony Miller <anthonymdev@gmail.com>
2022-08-23 15:35:23 -0700

    WIP setting up plugin to call CLI tool

--- Plugins/CodegenPlugin/CodegenPlugin.swift
+++ Plugins/CodegenPlugin/CodegenPlugin.swift
@@ -3,6 +3,10 @@ import PackagePlugin
 
 @main struct CodegenPlugin: CommandPlugin {
   func performCommand(context: PluginContext, arguments: [String]) async throws {
-    print("It's all linked up!")
+    let tool = try context.tool(named: "CodegenCLI")
+    let toolExecUrl = URL(fileURLWithPath: tool.path.string)
+
+    let process = try Process.run(toolExecUrl, arguments: ["generate"])
+    process.waitUntilExit()    
   }
 }
